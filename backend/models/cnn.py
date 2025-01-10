7fae5fbb51709cb0f69d1e7ff12b7bcb9d530c8a
ga111o <ga111o@proton.me>
2025-01-10 12:24:10 +0900

    cnn 돌리기 전에 데이터 증강 하도록

--- backend/models/cnn.py
+++ backend/models/cnn.py
@@ -11,7 +11,16 @@ def train_model(image_dir='./images/'):
     if not os.path.exists(image_dir):
         raise FileNotFoundError("not os.path.exists(image_dir):")
 
-    data_gen = ImageDataGenerator(rescale=1./255)
+    data_gen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest'
+    )
 
     data = data_gen.flow_from_directory(
         image_dir,
